4d4c09c876c666773e0005c7ecc671634d0d222d
Claire Donnat <cdonnat@Claires-MacBook-Pro.local>
2021-06-09 11:41:23 -0700

    adding helper functions

---
+++ Experiments/helper.py
@@ -0,0 +1,82 @@
+def visualize_graph(G, color, size=300, epoch=None, loss=None):
+    plt.figure(figsize=(7,7))
+    plt.xticks([])
+    plt.yticks([])
+    nx.draw_networkx(G, pos=nx.spring_layout(G, seed=42), with_labels=False,
+                         node_color=color, cmap="Set2", node_size = size)
+    plt.show()
+    
+def visualize_tsne(out, color, size=30, epoch=None, loss = None):
+    z = TSNE(n_components=2).fit_transform(out.detach().cpu().numpy())
+
+    plt.figure(figsize=(7,7))
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.scatter(z[:, 0], z[:, 1], s=size, c=color, cmap="Set2")
+    if epoch is not None and loss is not None:
+        plt.xlabel(f'Epoch: {epoch}, Loss: {loss:.4f}', fontsize=16)
+    plt.show()
+    
+def visualize_pca(out, color, size=30, epoch=None, loss=None):
+    h = PCA(n_components=2).fit_transform(out.detach().cpu().numpy())
+    plt.figure(figsize=(7,7))
+    plt.xticks([])
+    plt.yticks([])
+    
+    plt.scatter(h[:, 0], h[:, 1], s=size, c=color, cmap="Set2")    
+    if epoch is not None and loss is not None:
+        plt.xlabel(f'Epoch: {epoch}, Loss: {loss:.4f}', fontsize=16)
+    plt.show()
+    
+def delete_undirected_edges(edge_index, p):
+    edges = torch.clone(edge_index).t().numpy()
+    edges = set(map(frozenset, edges))
+    n = len(edges)
+    edges = random.sample(edges, round(n*(1-p)))
+    edges = list(map(list, edges))
+    reverse_edges = list(map(lambda x: [x[1],x[0]], edges))
+    sample = sorted(edges + reverse_edges)
+    return torch.tensor(sample).t().contiguous()
+
+def add_undirected_edges(edge_index, edge_num, node_total = 2708):
+    edges = torch.clone(edge_index).t().numpy()
+    edges = set(map(frozenset, edges))
+    n = len(edges)
+    while len(edges) - n < edge_num:
+        edges.add(frozenset(random.sample(range(node_total), 2)))
+    edges = list(map(list, edges))
+    reverse_edges = list(map(lambda x: [x[1],x[0]], edges))
+    sample = sorted(edges + reverse_edges)
+    return torch.tensor(sample).t().contiguous()
+
+def dataset_print(dataset):
+    print(f'Dataset: {dataset}:')
+    print('======================')
+    print(f'Number of graphs: {len(dataset)}')
+    print(f'Number of features: {dataset.num_features}')
+    print(f'Number of classes: {dataset.num_classes}')
+    
+def data_print(data):
+    print(data)
+    print('==============================================================')
+
+    # Gather some statistics about the graph.
+    print(f'Number of nodes: {data.num_nodes}')
+    print(f'Number of edges: {data.num_edges}')
+    print(f'Average node degree: {data.num_edges / data.num_nodes:.2f}')
+    print(f'Number of training nodes: {data.train_mask.sum()}')
+    print(f'Training node label rate: {int(data.train_mask.sum()) / data.num_nodes:.2f}')
+    print(f'Contains isolated nodes: {data.contains_isolated_nodes()}')
+    print(f'Contains self-loops: {data.contains_self_loops()}')
+    print(f'Is undirected: {data.is_undirected()}')
+
+def timethis(func):
+    @wraps(func) 
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print("Time to run function '{}': {:.2f} seconds".format(func.__name__, end-start)) 
+        return result
+    return wrapper
